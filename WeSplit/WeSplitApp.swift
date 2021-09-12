572948c331ae32c3c7b93a62596a301d0a9560b3
Truc Phan <pttruc.308@gmail.com>
2021-09-11 20:28:31 -0700

    first commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Truc Phan on 9/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
